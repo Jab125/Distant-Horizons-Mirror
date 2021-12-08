407a9174fbea7b2f23e51a5ed349da43c911a01a
tom lee <tomlee92502@yahoo.com>
2021-12-08 17:13:33 +0800

    GLProxy: Fixed Shader null error on WindowsOS
    The cause is using OS seperator instead of "/" java resource seperator
    when using relative path

--- src/main/java/com/seibel/lod/core/render/GLProxy.java
+++ src/main/java/com/seibel/lod/core/render/GLProxy.java
@@ -255,8 +255,9 @@ public class GLProxy
 		try
 		{
 			// get the shaders from the resource folder
-			vertexShader = LodShader.loadShader(GL20.GL_VERTEX_SHADER, "shaders" + File.separator + "standard.vert", false);
-			fragmentShader = LodShader.loadShader(GL20.GL_FRAGMENT_SHADER, "shaders" + File.separator + "flat_shaded.frag", false);
+			// Use File.separator ONLY if the file is external (not in the resourse), otherwise use "/"
+			vertexShader = LodShader.loadShader(GL20.GL_VERTEX_SHADER, "shaders/standard.vert", false);
+			fragmentShader = LodShader.loadShader(GL20.GL_FRAGMENT_SHADER, "shaders/flat_shaded.frag", false);
 			
 			// this can be used when testing shaders, 
 			// since we can't hot swap the files in the resource folder 
