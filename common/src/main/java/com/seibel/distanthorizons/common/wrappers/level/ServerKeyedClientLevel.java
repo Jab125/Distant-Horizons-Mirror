c46c056980059312b197ae6bba7c068f9191f0ec
s809 <43530948+s809@users.noreply.github.com>
2025-08-16 20:59:34 +0500

    Add a server keys feature

--- common/src/main/java/com/seibel/distanthorizons/common/wrappers/level/ServerKeyedClientLevel.java
+++ common/src/main/java/com/seibel/distanthorizons/common/wrappers/level/ServerKeyedClientLevel.java
@@ -2,24 +2,28 @@ package com.seibel.distanthorizons.common.wrappers.level;
 
 import com.seibel.distanthorizons.common.wrappers.world.ClientLevelWrapper;
 import com.seibel.distanthorizons.core.level.IServerKeyedClientLevel;
-import com.seibel.distanthorizons.core.wrapperInterfaces.world.ILevelWrapper;
-import com.seibel.distanthorizons.coreapi.util.StringUtil;
 import net.minecraft.client.multiplayer.ClientLevel;
 
 public class ServerKeyedClientLevel extends ClientLevelWrapper implements IServerKeyedClientLevel
 {
+	/** Returns the folder name the server wants the client to use. */
+	private final String serverKey;
+	
 	/** A unique identifier (generally the level's name) for differentiating multiverse levels */
 	private final String serverLevelKey;
 	
 	
 	
-	public ServerKeyedClientLevel(ClientLevel level, String serverLevelKey)
+	public ServerKeyedClientLevel(ClientLevel level, String serverKey, String serverLevelKey)
 	{
 		super(level);
+		this.serverKey = serverKey;
 		this.serverLevelKey = serverLevelKey;
 	}
 	
 	
+	@Override
+	public String getServerKey() { return this.serverKey; }
 	
 	@Override
 	public String getServerLevelKey() { return this.serverLevelKey; }
