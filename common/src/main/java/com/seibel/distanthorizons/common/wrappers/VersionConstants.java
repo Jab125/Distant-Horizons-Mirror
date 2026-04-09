f87afb34f4a2b15f76a21dc35d64c603aa83ba4d
James Seibel <jeseibel@gondtc.com>
2026-04-08 22:00:11 -0500

    Rename MC 1.26.1 -> 26.1.0

--- common/src/main/java/com/seibel/distanthorizons/common/wrappers/VersionConstants.java
+++ common/src/main/java/com/seibel/distanthorizons/common/wrappers/VersionConstants.java
@@ -93,8 +93,8 @@ public class VersionConstants implements IVersionConstants
 			return "1.21.10";
 		#elif MC_VER == MC_1_21_11
 			return "1.21.11";
-		#elif MC_VER == MC_1_26_1
-			return "21.6";
+		#elif MC_VER == MC_26_1_0
+			return "21.6.0";
 		#else
 			ERROR MC version constant missing
 		#endif
