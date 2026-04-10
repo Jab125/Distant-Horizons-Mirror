3b3be6aed4c31fc70de62b1df96d16446c0a576b
James Seibel <jeseibel@gondtc.com>
2026-04-10 07:50:15 -0500

    fix old MC compiling

--- neoforge/src/main/java/com/seibel/distanthorizons/neoforge/mixins/client/MixinGameRenderer.java
+++ neoforge/src/main/java/com/seibel/distanthorizons/neoforge/mixins/client/MixinGameRenderer.java
@@ -24,7 +24,7 @@ import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(Entity.class)
-public class MixinGamerenderer {}
+public class MixinGameRenderer {}
 
 #else
 import com.mojang.blaze3d.vertex.PoseStack;
