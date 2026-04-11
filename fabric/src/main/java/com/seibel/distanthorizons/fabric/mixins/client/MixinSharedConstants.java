b42d3d8f74249d1681915a46ee07c6c5ee352a63
James Seibel <jeseibel@gondtc.com>
2026-04-11 15:23:32 -0500

    remove Iris incompatibility and disable validation when present

--- fabric/src/main/java/com/seibel/distanthorizons/fabric/mixins/client/MixinSharedConstants.java
+++ fabric/src/main/java/com/seibel/distanthorizons/fabric/mixins/client/MixinSharedConstants.java
@@ -1,5 +1,7 @@
 package com.seibel.distanthorizons.fabric.mixins.client;
 
+import com.seibel.distanthorizons.core.logging.DhLogger;
+import com.seibel.distanthorizons.core.logging.DhLoggerBuilder;
 import com.seibel.distanthorizons.coreapi.ModInfo;
 import net.minecraft.SharedConstants;
 import org.spongepowered.asm.mixin.*;
@@ -16,8 +18,34 @@ public abstract class MixinSharedConstants
 	@Inject(method = "<clinit>", at = @At("TAIL"))
 	private static void setIsRunningInIde(CallbackInfo ci) 
 	{
-		// run extra validation for dev builds
+		DhLogger logger = new DhLoggerBuilder().name("SharedConstants").build();
+		
+		// setting IS_RUNNING_IN_IDE to true enables
+		// additional validation on Mojang's side which
+		// helps catch errors when developing for Blaze3D
+		
+		boolean irisPresent;
+		#if MC_VER <= MC_1_21_11
 		IS_RUNNING_IN_IDE = ModInfo.IS_DEV_BUILD;
+		#else
+		try
+		{
+			// Iris has a bug for MC 26 and newer where it doesn't have
+			// a "sampler1" bound, causing a renderer crash if
+			// Blaze3D validation is enabled (which is enabled by if
+			// IS_RUNNING_IN_IDE is true)
+			ModInfo.class.getClassLoader().loadClass("net.irisshaders.iris.api.v0.IrisApi");
+			irisPresent = true;
+		}
+		catch (ClassNotFoundException ignore)
+		{
+			irisPresent = false;
+		}
+		
+		IS_RUNNING_IN_IDE = ModInfo.IS_DEV_BUILD && !irisPresent;
+		#endif
+		
+		logger.info("Setting Minecraft's SharedConstants.IS_RUNNING_IN_IDE to ["+IS_RUNNING_IN_IDE+"]");
 	}
 	
 }
