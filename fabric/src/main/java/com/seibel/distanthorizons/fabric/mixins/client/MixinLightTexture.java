ef796081f7a13718bc2b9991986e13c89ca3681d
James Seibel <jeseibel@gondtc.com>
2026-09-12 10:12:44 -0500

    light texture blaze imports

--- fabric/src/main/java/com/seibel/distanthorizons/fabric/mixins/client/MixinLightTexture.java
+++ fabric/src/main/java/com/seibel/distanthorizons/fabric/mixins/client/MixinLightTexture.java
@@ -40,11 +40,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import com.mojang.blaze3d.platform.NativeImage;
 #elif MC_VER < MC_1_21_5
 import com.mojang.blaze3d.pipeline.TextureTarget;
-#else
+#elif MC_VER <= MC_26_2_0
 import com.mojang.blaze3d.opengl.GlTexture;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.textures.GpuTexture;
+#else
+import com.mojang.renderpearl.api.textures.GpuTexture;
+import com.mojang.renderpearl.backend.opengl.GlTexture;
 #endif
+
 #if MC_VER <= MC_1_21_11
 import net.minecraft.client.renderer.LightTexture;
 #else
