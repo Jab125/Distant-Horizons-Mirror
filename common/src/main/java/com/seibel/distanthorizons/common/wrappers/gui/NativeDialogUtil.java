8192777aa131667ec43e6786e2d5cf51e11abbe9
James Seibel <jeseibel@gondtc.com>
2026-09-12 09:55:23 -0500

    Use JSwing for MC 26.3 dialogs

--- common/src/main/java/com/seibel/distanthorizons/common/wrappers/gui/NativeDialogUtil.java
+++ common/src/main/java/com/seibel/distanthorizons/common/wrappers/gui/NativeDialogUtil.java
@@ -1,19 +1,23 @@
 package com.seibel.distanthorizons.common.wrappers.gui;
 
+#if MC_VER <= MC_26_2_0
 import org.lwjgl.util.tinyfd.TinyFileDialogs;
+#else
+import javax.swing.*;
+#endif
 
 /**
- * Should be used instead of the direct call to {@link TinyFileDialogs}
- * so we can run additional validation and/or string cleanup.
- * Otherwise, we may get error messages back. <br><br>
+ * Should be used instead of the direct call to TinyFileDialogs
+ * so we can run additional validation, string cleanup,
+ * and use different backends for MC versions that
+ * don't support TinyFileDialogs. <br><br>
  * 
  * source:
  * https://sourceforge.net/projects/tinyfiledialogs/
- * 
- * @see TinyFileDialogs
  */
 public class NativeDialogUtil
 {
+	// TODO these string options should be replaced with an enum
 	/**
 	 * @param dialogType    the dialog type. One of:<br><table><tr><td>"ok"</td><td>"okcancel"</td><td>"yesno"</td><td>"yesnocancel"</td></tr></table>
 	 * @param iconType      the icon type. One of:<br><table><tr><td>"info"</td><td>"warning"</td><td>"error"</td><td>"question"</td></tr></table>
@@ -32,10 +36,50 @@ public class NativeDialogUtil
 		TinyFileDialogs.tinyfd_messageBox(title, message, dialogType, iconType, 1 /* ok/yes */);
 		#elif MC_VER <= MC_1_21_11
 		TinyFileDialogs.tinyfd_messageBox(title, message, dialogType, iconType, false);
-		#else
+		#elif MC_VER <= MC_26_2_0
 		// https://mfbridge.github.io/tinyfiledialogs/reference/messageBox.html
 		TinyFileDialogs.tinyfd_messageBox(title, message, dialogType, iconType, 1 /* ok/yes */);
+		#else
+		// Java swing is being used because MC removed TinyFileDialogs in MC 26.3.0.
+		// This is known not to work on Mac, so if that becomes an issue in the future
+		// we may want to look into embedding our own copy of TinyFileDialogs.
+		int messageType;
+		switch (iconType)
+		{
+			case "error":
+				messageType = JOptionPane.ERROR_MESSAGE;
+				break;
+			case "warning":
+				messageType= JOptionPane.WARNING_MESSAGE;
+				break;
+			case "info":
+				messageType = JOptionPane.INFORMATION_MESSAGE;
+				break;
+			case "question":
+				messageType = JOptionPane.QUESTION_MESSAGE;
+				break;
+			default:
+				messageType = JOptionPane.PLAIN_MESSAGE;
+				break;
+		};
+		JOptionPane.showConfirmDialog(null, message, title, JOptionPane.DEFAULT_OPTION, messageType);
 		#endif
 	}
+	
+	/** 
+	 * if we're using Java Swing for our UI
+	 * dialogs then AWT needs to be set to "false"
+	 * otherwise the AWT setting doesn't matter.
+	 */
+	public static boolean needsAwtHeadless()
+	{
+		#if MC_VER <= MC_1_12_2
+		return false;
+		#else
+		return true;
+		#endif
+	}
+	
+	
 	
 }
--- common/src/main/java/com/seibel/distanthorizons/common/wrappers/minecraft/MinecraftClientWrapper.java
+++ common/src/main/java/com/seibel/distanthorizons/common/wrappers/minecraft/MinecraftClientWrapper.java
@@ -576,6 +576,9 @@ public class MinecraftClientWrapper extends AbstractMinecraftSharedWrapper imple
 	public void showDialog(String title, String message, String dialogType, String iconType)
 	{ NativeDialogUtil.showDialog(title, message, dialogType, iconType); }
 	
+	@Override
+	public boolean dialogNeedsAwtHeadless() { return NativeDialogUtil.needsAwtHeadless(); }
+	
 	//endregion
 	
 	
