07c0f451909711e7bba838f67f9ce58c30993fb1
James Seibel <jeseibel@gondtc.com>
2026-09-16 07:38:17 -0500

    Fix 1.12 not allow world gen disabling

--- common/src/main/java/com/seibel/distanthorizons/common/AbstractModInitializer.java
+++ common/src/main/java/com/seibel/distanthorizons/common/AbstractModInitializer.java
@@ -536,7 +536,6 @@ public abstract class AbstractModInitializer
 		// graphics/rendering
 		#if MC_VER <= MC_1_12_2
 		Config.Client.Advanced.Graphics.Experimental.renderingEngine.setMcVersionOverrideValue(EDhApiRenderingEngine.OPEN_GL);
-		Config.Common.WorldGenerator.generatorPlan.setMcVersionOverrideValue(EDhApiGeneratorPlan.CHUNKS_ONLY);
 		Config.Common.WorldGenerator.chunkGeneratorMode.setMcVersionOverrideValue(EDhApiDistantGeneratorMode.INTERNAL_SERVER);
 		#elif MC_VER <= MC_1_21_10
 		Config.Client.Advanced.Graphics.Experimental.renderingEngine.setMcVersionOverrideValue(EDhApiRenderingEngine.OPEN_GL);
