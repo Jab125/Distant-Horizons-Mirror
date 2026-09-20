82ff0f538995a0291d56cb71e88d3b37f64f3e2d
Vojtěch Šokala <vojtasokala@gmail.com>
2026-09-20 11:47:55 +0200

    Fix crash when iris fork and gregtech is present on 1.12.2

--- cleanroom/src/main/java/com/seibel/distanthorizons/cleanroom/mixins/client/MixinBloomEffectUtil.java
+++ cleanroom/src/main/java/com/seibel/distanthorizons/cleanroom/mixins/client/MixinBloomEffectUtil.java
@@ -1,11 +1,12 @@
 package com.seibel.distanthorizons.cleanroom.mixins.client;
 
-import com.seibel.distanthorizons.common.commonMixins.IFramebufferDepthTexture;
+import com.seibel.distanthorizons.common.wrappers.minecraft.MinecraftRenderWrapper;
+import com.seibel.distanthorizons.core.dependencyInjection.SingletonInjector;
 import gregtech.client.utils.BloomEffectUtil;
 import gregtech.client.utils.RenderUtil;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.shader.Framebuffer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -15,7 +16,8 @@ public class MixinBloomEffectUtil
 	@Redirect(method = "renderBloomInternal", at = @At(value = "INVOKE", target = "Lgregtech/client/utils/RenderUtil;hookDepthBuffer(Lnet/minecraft/client/shader/Framebuffer;I)V"))
 	private static void distantHorizons$hookDepthTextureInstead(Framebuffer fbo, int depthBuffer)
 	{
-		int dhDepthTex = ((IFramebufferDepthTexture) Minecraft.getMinecraft().getFramebuffer()).distantHorizons$getDistantHorizonsDepthTexture();
+		int dhDepthTex = MinecraftRenderWrapper.INSTANCE.getGlDepthTextureId();
+		
 		if (dhDepthTex != -1)
 		{
 			RenderUtil.hookDepthTexture(fbo, dhDepthTex);
@@ -25,4 +27,5 @@ public class MixinBloomEffectUtil
 			RenderUtil.hookDepthBuffer(fbo, depthBuffer);
 		}
 	}
+	
 }
